f0f0fc127df5e2c85a0e683f4b2a0b358a2aca1b
Tom de Grunt <tom@degrunt.nl>
2020-12-09 13:35:32 +0100

    Be less loud, don't run job if there's no need

--- app/channels/signum/appearance_channel.rb
+++ app/channels/signum/appearance_channel.rb
@@ -5,23 +5,19 @@ require_dependency 'signum/application_cable/channel'
 module Signum
   class AppearanceChannel < ApplicationCable::Channel
     def subscribed
-      Rails.logger.info('subscribed')
       current_user.online!
     end
 
     def unsubscribed
-      Rails.logger.info('unsubscribed')
       current_user.offline!
     end
 
     def appear(_data)
-      Rails.logger.info('appear')
       current_user.appeared! # (on: data['appearing_on'])
-      Signum::SendSignalsJob.perform_later(current_user)
+      Signum::SendSignalsJob.perform_later(current_user) if current_user.signals.pending.count >= 1
     end
 
     def away
-      Rails.logger.info('away')
       current_user.away!
     end
   end
