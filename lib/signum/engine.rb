577c75ef6923e96ead4172d91dd2a455fde1bf21
shubh <S.Seetharamasetty@rapidlogistics.nl>
2023-12-28 01:07:45 +0400

    append_migration

--- lib/signum/engine.rb
+++ lib/signum/engine.rb
@@ -1,5 +1,12 @@
 module Signum
   class Engine < ::Rails::Engine
     isolate_namespace Signum
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths['db/migrate'].expanded.each do |expanded_path|
+          app.config.paths['db/migrate'] << expanded_path
+        end
+      end
+    end
   end
 end
