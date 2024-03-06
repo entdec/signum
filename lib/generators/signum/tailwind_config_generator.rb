ebde83012ae2f5cba74b5322d13ce8bffab52d99
Tom de Grunt <tom@degrunt.nl>
2024-03-06 14:40:07 +0100

    Use *

--- lib/generators/signum/tailwind_config_generator.rb
+++ lib/generators/signum/tailwind_config_generator.rb
@@ -9,7 +9,7 @@ module Signum
       def add_content_to_tailwind_config
         inject_into_file "config/tailwind.config.js", before: "],\n  theme: {" do
           "  // Signum content\n" +
-            %w[/app/views/**/* /app/helpers/**/* /app/controllers/**/* /app/components/**/* /app/javascript/**/*.js /app/assets/**/application.css].map { |path| "    \"#{Signum::Engine.root}#{path}\"" }.join(",\n") +
+            %w[/app/views/**/* /app/helpers/**/* /app/controllers/**/* /app/components/**/* /app/javascript/**/*.js /app/assets/**/*.css].map { |path| "    \"#{Signum::Engine.root}#{path}\"" }.join(",\n") +
             ",\n  "
         end
       end
