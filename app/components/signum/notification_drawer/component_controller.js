0c899a18d6b2972e65f1421400852a52a1baa7a1
Syed Waqas Ali Shah <s.waqas@rapidlogistics.nl>
2024-03-15 09:20:35 +0400

    reformat js

--- app/components/signum/notification_drawer/component_controller.js
+++ app/components/signum/notification_drawer/component_controller.js
@@ -1,48 +1,48 @@
-import ApplicationController from "signum/controllers/application_controller";
+import ApplicationController from "signum/controllers/application_controller"
 
 export default class extends ApplicationController {
-  static targets = ["alertbellicon", "bellicon", "submenu", "item"];
+  static targets = ["alertbellicon", "bellicon", "submenu", "item"]
 
   connect() {
-    this.bounditemActivity = this.itemActivity.bind(this);
-    window.addEventListener("nd-item-activity", this.bounditemActivity);
+    this.bounditemActivity = this.itemActivity.bind(this)
+    window.addEventListener("nd-item-activity", this.bounditemActivity)
 
     setTimeout(() => {
-      this.manageBellIcon();
-    }, 300);
+      this.manageBellIcon()
+    }, 300)
   }
 
   disconnect() {
-    window.removeEventListener("nd-item-activity", this.bounditemActivity);
+    window.removeEventListener("nd-item-activity", this.bounditemActivity)
   }
 
   show(event) {
     if (this.submenuTarget.children.length > 0) {
-      this.submenuTarget.classList.remove("hidden");
+      this.submenuTarget.classList.remove("hidden")
     } else {
-      this.submenuTarget.classList.add("hidden");
+      this.submenuTarget.classList.add("hidden")
     }
   }
 
   hide(event) {
-    this.submenuTarget.classList.add("hidden");
+    this.submenuTarget.classList.add("hidden")
   }
 
   itemActivity(event) {
-    this.manageBellIcon();
+    this.manageBellIcon()
 
     if (this.submenuTarget.children.length == 0) {
-      this.hide(event);
+      this.hide(event)
     }
   }
 
   manageBellIcon() {
     if (this.submenuTarget.children.length > 0) {
-      this.alertbelliconTarget.classList.remove("hidden");
-      this.belliconTarget.classList.add("hidden");
+      this.alertbelliconTarget.classList.remove("hidden")
+      this.belliconTarget.classList.add("hidden")
     } else {
-      this.alertbelliconTarget.classList.add("hidden");
-      this.belliconTarget.classList.remove("hidden");
+      this.alertbelliconTarget.classList.add("hidden")
+      this.belliconTarget.classList.remove("hidden")
     }
   }
 }
