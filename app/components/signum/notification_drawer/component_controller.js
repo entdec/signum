06c9955adc373cc7708dcba10a5dc00c748a3819
Twisted Clone <dave@boxture.com>
2024-04-09 14:55:15 +0200

    Javascript changes

--- app/components/signum/notification_drawer/component_controller.js
+++ app/components/signum/notification_drawer/component_controller.js
@@ -8,42 +8,56 @@ export default class extends ApplicationController {
     this.bounditemActivity = this.itemActivity.bind(this)
     window.addEventListener("nd-item-activity", this.bounditemActivity)
 
+    this.drawerButtonHoverIn = () => this.showCrossIcon();
+    this.drawerButtonHoverOut = () => this.hideCrossIcon();
+
+    this.findDrawerButton().addEventListener("mouseover", this.drawerButtonHoverIn);
+    this.findDrawerButton().addEventListener("mouseleave", this.drawerButtonHoverOut)
+
     setTimeout(() => {
       this.manageBellIcon()
     }, 300)
-
   }
 
   disconnect() {
     window.removeEventListener("nd-item-activity", this.bounditemActivity)
+
+
+    const button = this.findDrawerButton();
+    if (button) {
+      button.removeEventListener("mouseover", this.drawerButtonHoverIn);
+      button.removeEventListener("mouseleave", this.drawerButtonHoverOut);
+    }
+  }
+
+  findDrawerButton() {
+    return this.element.querySelector('.signum-notification-drawer-button');
   }
 
   show(event) {
     if (this.submenuTarget.children.length > 0) {
       this.submenuTarget.classList.remove("hidden")
-      if (event.altKey) {
-        this.crossiconTarget.style.display = 'inline-block'
-      } else {
-        this.crossiconTarget.style.display = 'none'
-      }
     } else {
       this.submenuTarget.classList.add("hidden")
     }
   }
 
+  showCrossIcon() {
+    this.crossiconTarget.classList.remove("hidden");
+  }
+
+  hideCrossIcon() {
+    this.crossiconTarget.classList.add("hidden");
+  }
+
   hide(event) {
     this.submenuTarget.classList.add("hidden")
-    this.crossiconTarget.style.display = 'none'
     if (this.submenuTarget.children.length < 0) {
       this.alertbelliconTarget.classList.add("hidden")
-    }
-    if (event.altKey) {
-      this.crossiconTarget.style.display = 'none'
     }
   }
 
   closeNotifications(event) {
-    if (event.altKey) {
       fetch("/signal/close_all", {
         method: "GET",
         headers: {
@@ -54,10 +68,8 @@ export default class extends ApplicationController {
         .catch((err) => {
           console.log(err)
         })
-      this.crossiconTarget.style.display = 'none';
       this.belliconTarget.style.display = 'inline-block'
       this.alertbelliconTarget.style.display = 'none';
-    }
   }
 
   itemActivity(event) {
