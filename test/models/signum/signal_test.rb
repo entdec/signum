9669214cd6d4a78697dacbf7d685fcbd01376380
Tom de Grunt <tom@degrunt.nl>
2024-04-10 12:04:49 +0200

    Actually test something!

--- test/models/signum/signal_test.rb
+++ test/models/signum/signal_test.rb
@@ -5,6 +5,9 @@ module Signum
     test "creates a signal" do
       subject = Signum.success(users(:one), text: 'test')
       assert subject
+      assert_equal users(:one), subject.signalable
+      assert_equal "success", subject.kind
+      assert_equal "test", subject.text
     end
   end
 end
