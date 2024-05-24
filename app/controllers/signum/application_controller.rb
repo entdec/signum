23fbdd04de7b5e6ba75fe6fd1e8f84436e7fb3cd
Tom de Grunt <tom@degrunt.nl>
2024-05-24 13:09:47 +0200

    Not needed

--- app/controllers/signum/application_controller.rb
+++ app/controllers/signum/application_controller.rb
@@ -1,6 +1,5 @@
 module Signum
   class ApplicationController < Signum.config.base_controller.constantize
     protect_from_forgery with: :exception
-    include Signum.config.admin_authentication_module.constantize if Signum.config.admin_authentication_module
   end
 end
